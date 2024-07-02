971b405c0d61758223317c53b00784f2de80edbc
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2024-07-02 17:06:10 -0300

    Setup spring

---
+++ src/main/java/com/serioussam/flickplay/FlickplayApplication.java
@@ -0,0 +1,13 @@
+package com.serioussam.flickplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serioussam/flickplay/FlickplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serioussam.flickplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
